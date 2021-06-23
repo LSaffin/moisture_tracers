cc0605d6089e90a5c807622c58b6adebb1c09f55
Leo Saffin <lsaffin@exppostproc01.monsoon-metoffice.co.uk>
2021-06-23 13:11:48 +0000

    Initial repository created from cookiecutter template

---
+++ moisture_tracers/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for moisture_tracers."""
+
+__author__ = """Leo Saffin"""
+__email__ = 'l.saffin@leeds.ac.uk'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Leo Saffin",
+    author_email='l.saffin@leeds.ac.uk',
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',        
+    ],
+    description="Scripts for working with output from UM simulations with moisture tracers",
+    install_requires=requirements,
+    license="MIT license",
+    include_package_data=True,
+    keywords='moisture_tracers',
+    name='moisture_tracers',
+    packages=find_packages(include=['moisture_tracers', 'moisture_tracers.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/LSaffin/moisture_tracers',
+    version='0.1.0',
+    zip_safe=False,
+)
