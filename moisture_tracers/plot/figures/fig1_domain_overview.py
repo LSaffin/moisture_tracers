ff803149382e76bed1f3a87ef437b68d61b84b88
Leo Saffin <string_buster@hotmail.com>
2022-11-02 14:32:26 +0000

    Add vertical resolution to domain overview

--- moisture_tracers/plot/figures/fig1_domain_overview.py
+++ moisture_tracers/plot/figures/fig1_domain_overview.py
@@ -3,6 +3,7 @@ on the same field from the 1.1km simulation providing the boundary conditions
 """
 import warnings
 
+import numpy as np
 import iris.plot as iplt
 import matplotlib.pyplot as plt
 import cmcrameri
@@ -11,6 +12,7 @@ import irise
 from twinotter.external import eurec4a
 
 from moisture_tracers import datadir, plotdir
+from moisture_tracers.plot.figures import z_levs
 
 variable = "total_column_water"
 vmin, vmax = 20, 50
@@ -27,8 +29,17 @@ def main():
     )
     cube_1km = cube_1km.extract_cube(variable)
 
-    plt.figure(figsize=(8, 6))
+    fig, ax_dict = plt.subplot_mosaic(
+        """
+        aaaaaaaaaaaa
+        aaaaaaaaaaaa
+        aaaaaaaaaaaa
+        .bbbbbbbbb..
+        """,
+        figsize=(8, 8),
+    )
 
+    plt.axes(ax_dict["a"])
     for cube, cmap in [(cube_1km, "cmc.grayC"), (cube_500m, "cmc.oslo_r")]:
         iplt.pcolormesh(cube, vmin=vmin, vmax=vmax, cmap=cmap)
 
@@ -40,7 +51,7 @@ def main():
     x = cube.coord("grid_longitude").points - 180
     y = cube.coord("grid_latitude").points
     plt.plot(
-        [x[0], x[-1], x[-1], x[0], x[0]], [y[0], y[0], y[-1], y[-1], y[0]], color="r"
+        [x[0], x[-1], x[-1], x[0], x[0]], [y[0], y[0], y[-1], y[-1], y[0]], color="k", lw=2,
     )
 
     ax = plt.gca()
@@ -50,9 +61,21 @@ def main():
     gl.ylabels_left = True
     for xc in range(40, 70, 5):
         plt.text(180 - xc, -0.5, r"{}$\degree$W".format(xc), ha="center", va="top")
+
+    ax.text(0.0, 1.05, "(a)", dict(fontsize="large"), transform=ax.transAxes)
     plt.title(
         "Total Column Water (500m and 1.1km)\n" r"12Z 2$^\mathrm{nd}$ Feb (T+36h)"
     )
+
+    z_rho = (z_levs[1:] + z_levs[:-1]) / 2
+    dz = z_rho[1:] - z_rho[:-1]
+    ax_dict["b"].plot(dz*1e3, z_levs[1:-1], "-kx")
+    ax_dict["b"].set_xlim(0, 250)
+    ax_dict["b"].set_ylim(0, 5)
+    ax_dict["b"].set_xlabel("Vertical grid spacing (m)")
+    ax_dict["b"].set_ylabel("Height (km)")
+    ax_dict["b"].grid(which="both")
+    ax_dict["b"].text(0.01, 0.9, "(b)", dict(fontsize="large"), transform=ax_dict["b"].transAxes)
 
     plt.savefig(plotdir + "fig1_domain_overview.png")
 
