3cd16b8f258925df0b07e7ed2ba53fb7f953f2b0
Leo Saffin <l.saffin@leeds.ac.uk>
2023-01-18 15:57:35 +0000

    More specific trajectory file naming to allow multiple Lagrangian grids per case

--- moisture_tracers/trajectory.py
+++ moisture_tracers/trajectory.py
@@ -1,7 +1,7 @@
 """
 
 Usage:
-    trajectory.py <path> <start_time> <resolution>
+    trajectory.py <path> <start_time> <resolution> <x0> <y0> <z0> <t0>
     trajectory.py (-h | --help)
 
 Arguments:
@@ -25,10 +25,14 @@ from pylagranto.datasets import MetUMStaggeredGrid
 from moisture_tracers import grey_zone_forecast
 
 
-def main(path, start_time, resolution):
-    x0 = 302.5
-    y0 = 13.5
-    z0 = 500
+# HALO: x0=302.283, y0=13.3
+# Ron Brown (2nd Feb): x0=305.5, y0=13.9
+# 24th Jan Case study: x0=302.5, y0=11.75, t0=T+24h
+
+def main(path, start_time, resolution, x0, y0, z0, t0):
+    x0, y0, z0 = float(x0), float(y0), float(z0)
+    t0 = int(t0)
+
     levels = ("height_above_reference_ellipsoid", [z0])
 
     forecast = grey_zone_forecast(
@@ -39,15 +43,38 @@ def main(path, start_time, resolution):
     times = list(forecast._loader.files)
     datasource = MetUMStaggeredGrid(forecast._loader.files, levels=levels)
 
-    traout = caltra.caltra(
-        trainp, times, datasource, fbflag=-1, tracers=["x_wind", "y_wind"]
-    )
+    if t0 == 1:
+        traout = caltra.caltra(
+            trainp, times, datasource, tracers=["x_wind", "y_wind"]
+        )
+    elif t0 == len(times):
+        traout = caltra.caltra(
+            trainp, times, datasource, fbflag=-1, tracers=["x_wind", "y_wind"]
+        )
+    else:
+        traout_fwd = caltra.caltra(
+            trainp, times[t0-1:], datasource, tracers=["x_wind", "y_wind"]
+        )
+        traout_bck = caltra.caltra(
+            trainp, times[:t0], datasource, fbflag=-1, tracers=["x_wind", "y_wind"]
+        )
+        traout = traout_bck + traout_fwd
 
     traout.save(
-        "trajectories_{}_{}_{}m.pkl".format(
-            forecast.start_time.strftime("%Y%m%d"), resolution, z0
+        "trajectories_{}_{}_{}E_{}N_{}m_T+{}.pkl".format(
+            forecast.start_time.strftime("%Y%m%d"),
+            resolution,
+            format_float_for_file(x0),
+            format_float_for_file(y0),
+            format_float_for_file(z0),
+            t0
         )
     )
+
+
+def format_float_for_file(x):
+    # Replace decimal point with a p (copying what was done for the UM files)
+    return str(x).replace(".", "p")
 
 
 if __name__ == "__main__":
