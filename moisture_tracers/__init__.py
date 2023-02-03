f6d3da6c81dcd04e2612f60245743d00cd54a0cb
Leo Saffin <l.saffin@leeds.ac.uk>
2023-02-03 13:54:24 +0000

    Add support for RMED-style output in grey_zone_forecast

--- moisture_tracers/__init__.py
+++ moisture_tracers/__init__.py
@@ -5,6 +5,7 @@ __email__ = "l.saffin@leeds.ac.uk"
 __version__ = "0.1.0"
 
 
+from math import floor
 import datetime
 
 import iris.exceptions
@@ -20,6 +21,7 @@ from myscripts.projects.eurec4a.moisture_tracers import datadir, plotdir
 # model-variables_YYYYMMDDTHHMM_T+HH.nc and
 # moisture-tracers_YYYYMMDDTHHMM_T+HH.nc
 model_filename = "*_{start_time}_T+{lead_time:02d}.nc"
+rmed_filename = "{start_time}Z_EUREC4A_ICfinal1km_{resolution}_{model_setup}_pver*{lead_time:03d}.pp"
 
 # YYYYMMDDTHHMM_DomainResolution_T+HH_coarse_grid.nc
 regridded_filename = "{start_time}_{resolution}_T+{lead_time:02d}_{grid}.nc"
@@ -33,8 +35,10 @@ def grey_zone_forecast(
     path=datadir + "regridded/",
     start_time="2020-02-01",
     resolution="km4p4",
-    lead_times=range(48 + 1),
+    lead_times=range(1, 48 + 1),
     grid="coarse_grid",
+    output_type="default",
+    model_setup=None,
 ):
     """Return an irise.forecast.Forecast for an individual grey-zone simulation
 
@@ -45,6 +49,11 @@ def grey_zone_forecast(
         lead_times (iterable): Sequence of lead times (in hours) in the requested
             simulation. Default is every hour from 1 to 48
         grid (str | None):
+        output_type (str): Default or RMED. Default is my original output in netCDF
+            files. RMED is for loading from .pp files that have used the "RMED
+            toolbox" for output.
+        model_setup (str): If using RMED files, the filenames also contain the model
+            setup used (e.g. CoMorph or GAL8).
 
     Returns:
         irise.forecast.Forecast:
@@ -54,32 +63,60 @@ def grey_zone_forecast(
 
     start_time_str = start_time.strftime("%Y%m%dT%H%M")
 
-    if grid is None:
-        mapping = {
-            start_time
-            + datetime.timedelta(hours=dt): [
-                path
-                + model_filename.format(
-                    start_time=start_time_str, lead_time=max(0, dt - 1)
-                ),
-            ]
-            for dt in lead_times
-        }
+    if output_type.lower() == "default":
+        if grid is None:
+            mapping = {
+                start_time
+                + datetime.timedelta(hours=dt): [
+                    path
+                    + model_filename.format(
+                        start_time=start_time_str, lead_time=max(0, dt - 1)
+                    ),
+                ]
+                for dt in lead_times
+            }
 
-    else:
-        mapping = {
-            start_time
-            + datetime.timedelta(hours=dt): [
-                path
-                + regridded_filename.format(
-                    start_time=start_time_str,
-                    resolution=resolution,
-                    lead_time=dt,
-                    grid=grid,
-                ),
-            ]
-            for dt in lead_times
-        }
+        else:
+            mapping = {
+                start_time
+                + datetime.timedelta(hours=dt): [
+                    path
+                    + regridded_filename.format(
+                        start_time=start_time_str,
+                        resolution=resolution,
+                        lead_time=dt,
+                        grid=grid,
+                    ),
+                ]
+                for dt in lead_times
+            }
+
+    elif output_type.lower() == "rmed":
+        if grid is None:
+            mapping = {
+                start_time + datetime.timedelta(hours=dt): [
+                    path + resolution + "/" + rmed_filename.format(
+                        start_time=start_time_str,
+                        resolution=resolution,
+                        model_setup=model_setup,
+                        lead_time=12 * floor(dt/12),
+                        grid=grid,
+                    )
+                ]
+                for dt in lead_times
+            }
+
+            for dt in lead_times:
+                if (dt % 12) == 0:
+                    mapping[start_time + datetime.timedelta(hours=dt)].append(
+                        path + resolution + "/" + rmed_filename.format(
+                            start_time=start_time_str,
+                            resolution=resolution,
+                            model_setup=model_setup,
+                            lead_time=dt - 12,
+                            grid=grid,
+                        )
+                    )
 
     forecast = Forecast(start_time, mapping)
     forecast._loader = _ApproxLoader(forecast._loader.files)
@@ -91,19 +128,18 @@ def specific_fixes(cubes):
     # centre of the C-grid). This should affect u, v, and density
     # Rename "height_above_reference_ellipsoid" to "altitude" as a lot of my code
     # currently assumes an altitude coordinate
-    if 3 not in [cube.ndim for cube in cubes]:
-        return
-
     regridded = []
     example_cube = cubes.extract_cube("upward_air_velocity")
-    z = example_cube.coord("atmosphere_hybrid_height_coordinate")
-    example_cube.coord("height_above_reference_ellipsoid").rename("altitude")
-    for cube in cubes:
-        try:
-            cube.coord("atmosphere_hybrid_height_coordinate").units = "m"
-        except iris.exceptions.CoordinateNotFoundError:
-            pass
 
+    try:
+        z = example_cube.coord("atmosphere_hybrid_height_coordinate")
+        example_cube.coord("height_above_reference_ellipsoid").rename("altitude")
+    except iris.exceptions.CoordinateNotFoundError:
+        # Skip if we are not looking at height level data or
+        # height_above_reference_ellipsoid has already been renamed
+        return
+
+    for cube in cubes:
         if cube.ndim == 3:
             try:
                 cube.coord("height_above_reference_ellipsoid").rename("altitude")
