049f14f8f5eb94241b9c65bee63b295ffab8fc7c
Leo Saffin <string_buster@hotmail.com>
2023-06-13 14:40:34 +0100

    Use newly generated lagrangian domains for figures

--- moisture_tracers/plot/figures_parametrized_convection/__init__.py
+++ moisture_tracers/plot/figures_parametrized_convection/__init__.py
@@ -1,3 +1,23 @@
-from moisture_tracers import plotdir
+from moisture_tracers import plotdir, datadir, grey_zone_forecast
 
 plotdir = plotdir + "parametrized_convection/"
+
+forecasts = []
+for resolution in ["km1p1", "km4p4"]:
+    forecasts.append(grey_zone_forecast(
+        path=datadir + "regridded_vn12/",
+        start_time="2020-02-01",
+        resolution=resolution,
+        lead_times=range(1, 48+1),
+        grid="lagrangian_grid",
+    ))
+
+for model_setup in ["CoMorphA", "GAL8"]:
+    for resolution in ["km4p4", "km10"]:
+        forecasts.append(grey_zone_forecast(
+            path=datadir + "regridded_conv/",
+            start_time="2020-02-01",
+            resolution=f"{model_setup}_{resolution}",
+            lead_times=range(3, 48+1, 3),
+            grid="lagrangian_grid",
+        ))
--- moisture_tracers/plot/figures_parametrized_convection/fig1_satellite_comparison.py
+++ moisture_tracers/plot/figures_parametrized_convection/fig1_satellite_comparison.py
@@ -1,79 +1,53 @@
-import iris
-from iris.analysis import AreaWeighted
 import iris.plot as iplt
 import matplotlib.pyplot as plt
 import cmcrameri
 
-from moisture_tracers import grey_zone_forecast, datadir
-from moisture_tracers.plot.figures import lw_flux_plot_kwargs, projection
+from moisture_tracers.plot.figures import lw_flux_plot_kwargs
 from moisture_tracers.plot.figures.fig2_satellite_comparison import cbar_label
-from moisture_tracers.plot.figures_parametrized_convection import plotdir
+from moisture_tracers.plot.figures_parametrized_convection import plotdir, forecasts
 
 import warnings
 warnings.filterwarnings("ignore")
 
-labels_fc = ["RAL3\n1.1km", "RAL3\n4.4km"]
-labels_co = ["CoMorph\n4.4km", "CoMorph\n10km", "GAL8\n4.4km",  "GAL8\n10km"]
+labels_fc = [
+    "RAL3\n1.1km",
+    "RAL3\n4.4km",
+    "CoMorph\n4.4km",
+    "CoMorph\n10km",
+    "GAL8\n4.4km",
+    "GAL8\n10km"
+]
 
 height_factor = 5
 
 
 def main():
     varname = "toa_outgoing_longwave_flux"
-    lead_times = range(24, 48 + 1, 6)
-
-    cubes = []
-    # "{start_time}Z_EUREC4A_ICfinal1km_{resolution}_{model_setup}_pver*{lead_time:03d}.pp"
-    for model_setup in ["CoMorphA", "GAL8"]:
-        for resolution in ["km4p4", "km10"]:
-            cubes.append(iris.load_cube(
-                datadir + "{}/20200201T0000Z_EUREC4A_ICfinal1km_{}_{}_pvera*.pp".format(
-                    resolution, resolution, model_setup
-                ),
-                varname
-            ))
-
-    forecasts = []
-    for resolution in ["km1p1", "km4p4"]:
-        forecasts.append(grey_zone_forecast(
-            path=datadir + "regridded_vn12/",
-            start_time="2020-02-01",
-            resolution=resolution,
-            lead_times=lead_times,
-            grid="lagrangian_grid",
-        ))
-
-    times = forecasts[0].times
-    show_comparison(cubes, varname, forecasts, times, **lw_flux_plot_kwargs)
+    times = range(30, 48 + 1, 6)
+    show_comparison(varname, forecasts, times, cbar_label, **lw_flux_plot_kwargs)
     plt.savefig(plotdir + "comorph_comparison_lw.png")
 
 
-def show_comparison(cubes_co, varname, forecasts, times, **plot_kwargs):
-    nf = len(forecasts)
-    nc = len(cubes_co)
+def show_comparison(varname, forecasts_to_plot, times, cbar_labl, **plot_kwargs):
+    nf = len(forecasts_to_plot)
     nt = len(times)
 
-    nrows = height_factor * (nf + nc) + 1
+    nrows = height_factor * nf + 1
 
-    plt.figure(figsize=(8, nrows / 3))
+    plt.figure(figsize=(8, nrows / 4))
 
     for n, time in enumerate(times):
         print(time)
-        for m, forecast in enumerate(forecasts):
+        for m, forecast in enumerate(forecasts_to_plot):
             row = height_factor * m
-            plt.subplot2grid(
-                (nrows, nt),
-                (row, n),
-                rowspan=height_factor,
-                projection=projection,
-            )
+            plt.subplot2grid((nrows, nt), (row, n), rowspan=height_factor)
 
-            cubes = forecast.set_time(time)
+            cubes = forecast.set_lead_time(hours=time)
             cube_fc = cubes.extract_cube(varname)
-            iplt.pcolormesh(cube_fc, **plot_kwargs)
+            im = iplt.pcolormesh(cube_fc, **plot_kwargs)
 
             if m == 0:
-                plt.title(time.strftime("%HZ"))
+                plt.title(f"{time%24:02d}Z")
 
             # Put the resolution at the left side of each row
             if n == 0:
@@ -83,40 +57,9 @@ def show_comparison(cubes_co, varname, forecasts, times, **plot_kwargs):
                 ax.set_yticks([])
                 ax.set_ylabel(labels_fc[m])
 
-        for m2, cube_co in enumerate(cubes_co):
-            row = height_factor * (m2 + nf)
-            plt.subplot2grid(
-                (nrows, nt),
-                (row, n),
-                rowspan=height_factor,
-                projection=projection,
-            )
-
-            cube_co = cube_co.extract(iris.Constraint(
-                time=lambda cell: cell.point.hour == forecast.current_time.hour and
-                                  cell.point.day == forecast.current_time.day
-            ))
-            for axis in ["x", "y"]:
-                coord = cube_co.coord(axis=axis)
-                coord.guess_bounds()
-                coord.coord_system = cube_fc.coord(axis=axis).coord_system
-                coord.rename(cube_fc.coord(axis=axis).name())
-            cube_co = cube_co.regrid(cube_fc, AreaWeighted())
-
-            im = iplt.pcolormesh(cube_co, **plot_kwargs)
-
-            # Put the resolution at the left side of each row
-            if n == 0:
-                # Need to re-grab the axis because iplt.pcolormesh creates a new
-                # one
-                ax = plt.gca()
-                ax.get_yaxis().set_visible(True)
-                ax.set_yticks([])
-                ax.set_ylabel(labels_co[m2])
-
     ax = plt.subplot2grid((nrows, nt), (nrows - 1, 0), colspan=nt)
     cbar = plt.colorbar(im, cax=ax, orientation="horizontal")
-    cbar.set_label(cbar_label)
+    cbar.set_label(cbar_labl)
 
 
 if __name__ == '__main__':
